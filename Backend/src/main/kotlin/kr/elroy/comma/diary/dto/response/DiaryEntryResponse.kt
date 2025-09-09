5e71de418068bf1731cd4f760237523975e5ecd4
ElroyKR <elroykr@gmail.com>
2025-09-09 18:07:12 +0900

    feat(be): DiaryEntryResponse에 도메인 dto 변환 함수 추가

--- Backend/src/main/kotlin/kr/elroy/comma/diary/dto/response/DiaryEntryResponse.kt
+++ Backend/src/main/kotlin/kr/elroy/comma/diary/dto/response/DiaryEntryResponse.kt
@@ -2,6 +2,7 @@ package kr.elroy.comma.diary.dto.response
 
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
+import kr.elroy.comma.diary.domain.DiaryEntry
 import kr.elroy.comma.diary.dto.AnnotationDto
 
 data class DiaryEntryResponse(
@@ -13,4 +14,19 @@ data class DiaryEntryResponse(
     val annotations: List<AnnotationDto>,
     val createdAt: LocalDateTime,
     val updatedAt: LocalDateTime?,
-)
+) {
+    companion object {
+        fun from(entity: DiaryEntry): DiaryEntryResponse {
+            return DiaryEntryResponse(
+                id = entity.id.value,
+                authorId = entity.author.id.value,
+                title = entity.title,
+                content = entity.content,
+                entryDate = entity.entryDate,
+                annotations = entity.annotations.toList(),
+                createdAt = entity.createdAt,
+                updatedAt = entity.updatedAt,
+            )
+        }
+    }
+}
