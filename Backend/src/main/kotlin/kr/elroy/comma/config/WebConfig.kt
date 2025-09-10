44e05e76ff20c800671b7526e7acce15cf2e99e7
ElroyKR <elroykr@gmail.com>
2025-09-10 23:27:48 +0900

    feat(be): Add custom LocalDateTime serializer and deserializer

---
+++ Backend/src/main/kotlin/kr/elroy/comma/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package kr.elroy.comma.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig() : WebMvcConfigurer {
+    @Bean
+    @Primary
+    fun objectMapper(): ObjectMapper {
+        return Jackson2ObjectMapperBuilder()
+            .modules(KotlinModule.Builder().build(), JavaTimeModule(), LocalDateTimeModule())
+            .build()
+    }
+}
