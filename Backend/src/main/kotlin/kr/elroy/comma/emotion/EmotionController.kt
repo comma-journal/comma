19a462cf02ee2bae87b34ec3944c578d7284f833
ElroyKR <elroykr@gmail.com>
2025-09-11 16:56:27 +0900

    refactor(be): rename EmotionResponse to EmotionDto and update related references

--- Backend/src/main/kotlin/kr/elroy/comma/emotion/EmotionController.kt
+++ Backend/src/main/kotlin/kr/elroy/comma/emotion/EmotionController.kt
@@ -1,13 +1,13 @@
 package kr.elroy.comma.emotion
 
-import kr.elroy.comma.emotion.dto.EmotionResponse
+import kr.elroy.comma.emotion.dto.EmotionDto
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class EmotionController(
     private val emotionService: EmotionService,
 ) : EmotionApi {
-    override fun getEmotions(): List<EmotionResponse> {
+    override fun getEmotions(): List<EmotionDto> {
         return emotionService.findAll()
     }
 }
